2fd2693e9085a7a15ac8f79eab3b805837e3dd3f
PrakharSharma888 <prakharentp2002@gmail.com>
2022-04-26 13:04:58 +0530

    Made my first dapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import {useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  const [greetings, doGreetings] = useState(null)
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
